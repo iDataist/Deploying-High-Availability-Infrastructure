56e9dc6d2e36b9098104691b7bb999db74f178fb
Hui Ren <huiren16@gmail.com>
2022-07-04 16:45:20 -0500

    Update ec2.tf

--- infra/zone2/ec2.tf
+++ infra/zone2/ec2.tf
@@ -12,7 +12,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0418070344eccebfc"
+   aws_ami            = "ami-06637285523cf4347"
    private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
    public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnet_ids
    vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
