9b86081d05130888ff39b294f0323b4fdfd49f12
Hui Ren <huiren16@gmail.com>
2022-07-04 08:04:20 -0500

    [UPDATE]: updated the number of EC2 instances and EKS nodes

--- infra/zone1/rds.tf
+++ infra/zone1/rds.tf
@@ -1,8 +1,8 @@
-module "project_rds_p" {
-  source = "./modules/rds-p"
-  private_subnet_ids = module.vpc.private_subnet_ids
-  vpc_id = module.vpc.vpc_id
-}
+# module "project_rds_p" {
+#   source = "./modules/rds-p"
+#   private_subnet_ids = module.vpc.private_subnet_ids
+#   vpc_id = module.vpc.vpc_id
+# }
 
 # module "project_rds_s" {
 #   source                  = "./modules/rds-s"
