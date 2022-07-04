658f6da92ec836250f47decd23b29bef8f8b6f50
Hui Ren <huiren16@gmail.com>
2022-07-04 14:32:32 -0500

    [UPDATE]: updated ec2 terraform scripts

--- infra/zone2/modules/ec2/ec2.tf
+++ infra/zone2/modules/ec2/ec2.tf
@@ -12,7 +12,8 @@ resource "aws_instance" "ubuntu" {
   count = var.instance_count
   instance_type = "t3.micro"
   key_name = "udacity"
-  # subnet_id = var.public_subnet_ids[0]
+  for_each   = toset(var.public_subnet_ids)
+  subnet_id = each.value
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
